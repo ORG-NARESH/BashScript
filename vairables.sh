da6f0d854dfa9ed39b1d60825e3a549d330a8a02
naresh <Naresh@gmail.com>
2025-03-08 22:45:51 +0530

    bash

--- vairables.sh
+++ vairables.sh
@@ -7,7 +7,7 @@ echo 'print the value of $a'
 
 # Dynamically running script using system variables such as date,who,ps -f
 TodayDate=$(date +%D)           
-echo -e "Today's date is" \e[33m$TodayDate\e[0m
+echo -e "Today's date is "\e[33m $TodayDate\e[0m" "
 PROCESS=$(who | wc -l)
 echo "running process are $PROCESS"
 echo -e "This system hostname is" \e[32m$(hostname)\e[0m
