5afc34ae0ed8f0e87e14ea43f72263ffbe3143c4
naresh <Naresh@gmail.com>
2025-03-08 21:32:33 +0530

    bash

--- shebang.sh
+++ shebang.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -e "\e[31mEnter your name:  \e[0m"
+echo -e "\e[31m\e[41mEnter your name:  \e[0m"
 echo -e "\e[32m\nHello\tNaresh\tHow are you\nIam fine\b\e[0m"
 echo -e "\e[33mGlad that your doing\b great\e[0m"
