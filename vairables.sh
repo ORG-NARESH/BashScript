e4a89d6f51d80d99b1f7d8455b8e2cdbb05321ff
naresh <Naresh@gmail.com>
2025-03-08 22:38:24 +0530

    bash

--- vairables.sh
+++ vairables.sh
@@ -8,5 +8,5 @@ echo 'print the value of $a'
 # Dynamically running script using system variables such as date,who,ps -f
 TodayDate=$(date +%D)           
 echo "Today's date is $TodayDate"
-PROCESS=$(who)
+PROCESS=$(who | wc)
 echo "running process are $PROCESS"
